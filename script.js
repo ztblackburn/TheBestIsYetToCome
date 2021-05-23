fc12f8f4a53a1bef1e09fd39213e0ac7e5f08135
Zachary T Blackburn <ztblackburn@hotmail.com>
2021-05-23 03:10:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 (function () {
-$("#hell-js").click(function() {
-    if($("body").hasClass("hell")){
-        $("body").removeClass("hell");
-    }
-    else {
-        $("body").addClass("hell");
-    }
-});
+    $("#hell-js").click(function() {
+        if($("body").hasClass("hell")){
+            $("body").removeClass("hell");
+        }
+        else {
+            $("body").addClass("hell");
+        }
+    });
 
-$("#raise-js").click(function() {
+    $("#raise-js").click(function() {
+        $("#ghost-two-js").finish().animate({
+            opacity: "1"
+        }, 1000);
+        $("#ghost-two-js").addClass("shown");
+        
+    });
 
-        $("#ghost-two-js").addClass("show");
-    
-});
-
-$("#ghost-two-js").click(function() {
-if($("#ghost-two-js").hasClass("show")){
-        $("#ghost-two-js").addClass("move-ghost");
-    }
-});
-
-
-
-
-
+    $("#ghost-two-js").click(function() {
+        if($("#ghost-two-js").hasClass("shown")){
+            $("#ghost-two-js").addClass("move-ghost");
+            $("#hell-js").addClass("move-ghost");
+            $(".stage").fadeOut(6000);
+        };
+    });
 }());
